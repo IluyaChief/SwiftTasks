c1ab70a08e71c696563397ce4dbd71a432c10874
Iluya Ivanuk <>
2021-12-26 21:03:40 +0500

    Create TextField and any View

---
+++ CalclulatorSwiftProject/CalclulatorSwiftProject/ViewController.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ResultLabel: UILabel!
+    
+    override func viewDidLoad() {        super.viewDidLoad()
+
+    }
+    
+}
---
+++ TextFieldTask/TextFieldTask/CalculatorProcess.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class CalculatorProcess: UIViewController {
+    
+}
